ec7fedd4c24883a936b0f13ed5314756b42c42d3
freej595 <freej595@multicore-107.ad.liu.se>
2025-11-28 11:13:55 +0100

    del uppgift 1 klar

--- lab7/lab7B.py
+++ lab7/lab7B.py
@@ -29,20 +29,29 @@ def traverse(tree: list,
     def right_subtree(tree: list):
         return tree[2]
 
+    def subtree_calc(subtree):
+        if isinstance(subtree(tree), list):
+            value = leaf_fn(traverse(subtree(tree),
+                                     inner_node_fn,
+                                     leaf_fn,
+                                     empty_tree_fn))
+        else:
+            value = leaf_fn(subtree(tree))
+
+        return value
+
     if not tree:
         return empty_tree_fn()
 
-    if isinstance(left_subtree(tree), list):
-        return traverse(left_subtree(tree),
-                        inner_node_fn, leaf_fn,
-                        empty_tree_fn)
-
+    left_value = subtree_calc(left_subtree)
+    right_value = subtree_calc(right_subtree)
+    
     return inner_node_fn(
         middle_subtree(tree),
-        leaf_fn(left_subtree(tree)),
-        leaf_fn(right_subtree(tree))
+        left_value,
+        right_value
     )
 
 
 if __name__ == "__main__":
-    print(traverse([[1, 2, 1], 7, 8], left_inner_node_fn, leaf_fn, empty_tree_fn))
+    print(traverse([[1,5,4], 7, [1,7,6]], left_inner_node_fn, leaf_fn, empty_tree_fn))
