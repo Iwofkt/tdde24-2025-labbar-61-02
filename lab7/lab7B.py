a2b664c7eb2ef4326d54337c0fed55b6fdc240fd
freej595 <freej595@multicore-107.ad.liu.se>
2025-11-28 11:48:33 +0100

    Contains key good

--- lab7/lab7B.py
+++ lab7/lab7B.py
@@ -1,4 +1,5 @@
 
+# Deluppgift 1
 def empty_tree_fn():
     return 0
 
@@ -13,6 +14,26 @@ def left_inner_node_fn(key, left_value, right_value):
 
 def right_inner_node_fn(key, left_value, right_value):
     return key + right_value
+
+
+# Deluppgift 2
+def contains_key(tree, wanted_key):
+    def empty_tree_fn():
+        return 0
+
+    def leaf_fn(key):
+        if key == wanted_key:
+            return True
+        if isinstance(key, bool):
+            return key
+        return False
+
+    def inner_node_fn(key, left_value, right_value):
+        if key == wanted_key or left_value or right_value:
+            return True
+        return False
+
+    return traverse(tree, inner_node_fn, leaf_fn, empty_tree_fn)
 
 
 def traverse(tree: list,
@@ -45,7 +66,7 @@ def traverse(tree: list,
 
     left_value = subtree_calc(left_subtree)
     right_value = subtree_calc(right_subtree)
-    
+
     return inner_node_fn(
         middle_subtree(tree),
         left_value,
@@ -55,3 +76,4 @@ def traverse(tree: list,
 
 if __name__ == "__main__":
     print(traverse([[1,5,4], 7, [1,7,6]], left_inner_node_fn, leaf_fn, empty_tree_fn))
+    print(contains_key([[1,5,4], 7, [1,8,[1,5,9]]], 10))
