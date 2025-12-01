b82e758359cfc86788cad5eb4d68fb2263621807
Fredrik Ejsing <fredrik.ejsing@gmail.com>
2025-12-01 11:01:07 +0100

    Work on tree_size

--- lab7/lab7B.py
+++ lab7/lab7B.py
@@ -36,6 +36,21 @@ def contains_key(tree, wanted_key):
     return traverse(tree, inner_node_fn, leaf_fn, empty_tree_fn)
 
 
+def tree_size(tree: list):
+    def empty_tree_fn():
+        return 0
+
+    def leaf_fn(key):
+        if empty_tree_fn():
+            return empty_tree_fn()
+        return 1
+
+    def inner_node_fn(key, left_value, right_value):
+        return 1 + left_value + right_value
+
+    return traverse(tree, inner_node_fn, leaf_fn, empty_tree_fn)
+
+
 def traverse(tree: list,
              inner_node_fn,
              leaf_fn,
@@ -77,3 +92,6 @@ def traverse(tree: list,
 if __name__ == "__main__":
     print(traverse([[1,5,4], 7, [1,7,6]], left_inner_node_fn, leaf_fn, empty_tree_fn))
     print(contains_key([[1,5,4], 7, [1,8,[1,5,9]]], 10))
+    print(tree_size([2, 7, []]))
+    print(tree_size([]))
+    print(tree_size([[1, 2, []], 4, [[], 5, 6]]))
