347f802d10933db21c7e4c20cc0e82f78234518b
Iwofkt <simon.8.johansson@gmail.com>
2025-12-04 11:14:49 +0100

    Working with tests

---
+++ lab7/test7A.py
@@ -0,0 +1,113 @@
+import unittest
+from lab7.lab7A import match, search
+from lab7.books import db
+
+class TestMathFunctions(unittest.TestCase):
+
+    def setUp(self):
+        # Very similar to db but with some parts substituted
+        # with "--" and "&"
+        self.pattern = [[['författare', ['john', 'zelle']],
+       ['titel', ['python', 'programming', 'an', 'introduction', 'to',
+                  'computer', 'science']],
+       "--"],
+      [['författare', ['armen', 'asratian']],
+       ['titel', ['diskret', 'matematik']],
+       "&"],
+      [['författare', ['j', 'glenn', 'brookshear']],
+       ['titel', ['computer', 'science', 'an', 'overview']],
+       ['År', 2011]],
+      [['författare', ['john', 'zelle']],
+       ['titel', ['data', 'structures', 'and', 'algorithms', 'using',
+                  'python', 'and', 'c++']],
+       ['År', 2009]],
+      [['författare', ['anders', 'haraldsson']],
+       ['&', ['programmering', 'i', 'lisp']],
+       ['År', 1993]]]
+
+    def test_match_basics(self):
+        """
+        Basic tests for our match function
+        """
+
+        # -- VALID TESTS -- #
+
+        # Exact match
+        pattern = ['författare', ['&', 'zelle']]
+        seq = ['författare', ['&', 'zelle']]
+        self.assertTrue(match(seq, pattern))
+
+        # -- INVALID TESTS -- #
+
+        # Test a mismatch
+        pattern = ['år', 'Hello']
+        seq = ['år', 'unknown']
+        self.assertFalse(match(seq, pattern))
+
+    def test_match_wildcard(self):
+        """
+        Specific tests for our wildcards
+        """
+
+        # -- VALID TESTS -- #
+
+        # Test with '--' for both a string and empty
+        pattern = ['titel', ['--', 'python', '--']]
+        seq = ['titel', ['python', 'rocks']]
+        self.assertTrue(match(seq, pattern))
+
+        # Test "&" substitution for a string
+        pattern = ['år', '&']
+        seq = ['år', '2023']
+        self.assertTrue(match(seq, pattern))
+
+        # Test with '--' covering multiple elements in the sequence
+        pattern = ['--', ['år', 2042], '--']
+        seq = ['something', ['år', 2042], 'more']
+        self.assertTrue(match(seq, pattern))
+
+        # Test with '&' matching an element and '__' att the same time
+        pattern = ['--', ['titel', ['&', '&']], '--']
+        seq = ['before', ['titel', ['python', 'rocks']], 'after']
+        self.assertTrue(match(seq, pattern))
+
+        # Test with '__' matching multiple elements
+        pattern = ['--']
+        seq = ['before', ['titel', ['python', 'rocks']], 'after']
+        self.assertTrue(match(seq, pattern))
+
+        # Test a big sequence and pattern
+        self.assertTrue(match(db, self.pattern))
+
+
+        # -- INVALID TESTS -- #
+
+        #Test so that '&' cant match with emptiness
+        pattern = ['--', ['titel', ['&', '&']], '--']
+        seq = ['before', ['titel', ['rocks']], 'after']
+        self.assertFalse(match(seq, pattern))
+
+        #Test so that '&' cant match with multiple elements
+        pattern = ['--', ['titel', ['&']], '--']
+        seq = ['before', ['titel', ['rocks', 'perfection']], 'after']
+        self.assertFalse(match(seq, pattern))
+
+    def test_search(self):
+
+        # -- VALID TESTS -- #
+
+        # Search with wildcards
+        pattern = ['--', ['titel', ['&', '&']], '--']
+        result = search(pattern, db)
+        self.assertEqual(result,[[['författare', ['armen', 'asratian']], ['titel', ['diskret', 'matematik']], ['År', 2012]], [['titel', ['diskret', 'matematik']], ['År', 2012]]])
+
+        # Search with wildcards
+        pattern = ['--', ['titel', ['&', '&']], '--']
+        result = search(pattern, db)
+        self.assertEqual(result,[[['författare', ['armen', 'asratian']], ['titel', ['diskret', 'matematik']], ['År', 2012]], [['titel', ['diskret', 'matematik']], ['År', 2012]]])
+
+        # -- INVALID TESTS -- #
+
+
+if __name__ == '__main__':
+    unittest.main()
